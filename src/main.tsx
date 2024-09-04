7079c7259be6a64610761aab374ae6b9a49e8314
Konstantin Zhirnov <kostya.zhirnov@gmail.com>
2024-09-04 14:29:03 -0700

    change links

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import App from './App.tsx'
 import './index.css'
 import { TonConnectUIProvider } from '@tonconnect/ui-react'
 
-const manifestUrl = 'https://Konstantin-Zhirnov.github.io/ton/tonconnect-manifest.json'
+const manifestUrl = 'https://konstantin-zhirnov.github.io/ton/tonconnect-manifest.json'
 
 createRoot(document.getElementById('root')!).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
