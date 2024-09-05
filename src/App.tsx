a10f1b32fa83ea67b7c5aaae7bc60466def44d15
Konstantin Zhirnov <kostya.zhirnov@gmail.com>
2024-09-04 19:56:20 -0700

    TWA SDK

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+import WebApp from '@twa-dev/sdk'
+
 import { Header } from './components/Header'
 import { Information } from './components/Information'
 import { Buttons } from './components/Buttons'
 
 function App() {
+  const showAlert = () => {
+    WebApp.showAlert('Hey there!')
+  }
   return (
     <>
       <Header />
       <main>
+        <b>{WebApp.platform}</b>
+        <button onClick={showAlert}>Show alert</button>
         <Information />
 
         <Buttons />
